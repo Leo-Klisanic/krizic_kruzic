7696a87e6f7953477ee30196291a405e42b6747d
Leo-Klisanic <64528179+Leo-Klisanic@users.noreply.github.com>
2020-05-31 21:20:49 +0200

    Dodane funkcije za klikanje
    Kad se klikne na pushbuttons one ce nesto pokazati

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -16,10 +16,45 @@ MainWindow::~MainWindow()
 
 void MainWindow::on_pushButton_clicked()
 {
-    ui->textEdit->setText("X");
+
 }
 
 void MainWindow::on_pushButton_2_clicked()
 {
-    ui->textEdit->setText("O");
+
+}
+
+void MainWindow::on_pushButton_3_clicked()
+{
+
+}
+
+void MainWindow::on_pushButton_4_clicked()
+{
+
+}
+
+void MainWindow::on_pushButton_5_clicked()
+{
+
+}
+
+void MainWindow::on_pushButton_6_clicked()
+{
+
+}
+
+void MainWindow::on_pushButton_7_clicked()
+{
+
+}
+
+void MainWindow::on_pushButton_8_clicked()
+{
+
+}
+
+void MainWindow::on_pushButton_9_clicked()
+{
+
 }
